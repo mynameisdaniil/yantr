b5fd6a82221f07739b4b245fe945ae1f1d34dfcc
Daniil Sobol <sobol.daniil@gmail.com>
2014-06-30 22:57:49 +0400

    basic working version

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-var yaff  = require('yaff');
+var Yaff  = require('yaff');
 var maybe = require('maybe2');
+var log   = console.log;
 
-var Yantr = module.exports = function Yantr() {
-}
+var Yantr = module.exports = function Yantr(tasks) {
+  if (!(this instanceof Yantr))
+    return new Yantr(tasks);
+  this.tasks = maybe(tasks).is(Array).getOrElse([]);
+  this.index = this.tasks.reduce(function (index, task) {
+    return maybe(task.tags).map(function (tags) {
+      tags.forEach(function (tag) {
+        if(!index[tag])
+          index[tag] = [];
+        index[tag].push(task);
+      });
+    }).getOrElse(index);
+  }, {});
+};
+
+Yantr.prototype.run = function (cb) {
+  var self = this;
+  Yaff(this.tasks).parEach(function (task) {
+      self.__taskExecutor(task, this);
+    }).finally(cb);
+};
+
+Yantr.prototype.__isDone = function (tag, cb) {
+  var self = this;
+  var done = this.index[tag].every(function (task) {
+    return task.done;
+  });
+  if (done)
+    return cb();
+  setImmediate(function () {
+    self.__isDone(tag, cb);
+  });
+};
+
+Yantr.prototype.__taskExecutor = function (task, cb) {
+  (function (task, self) {
+    Yaff(task.depends || []).parEach(function (dependency) {
+        self.__isDone(dependency, this);
+      }).seq(function () {
+        task.task(this);
+      }).seq(function () {
+        task.done = true;
+        this();
+      }).finally(cb);
+  })(task, this);
+};
--- test.js
+++ test.js
@@ -1,10 +1,52 @@
-var log = console.log();
+var log = console.log;
 
 var Yantr = require('./index.js');
 
 var tasks = [
+  {
+    depends: [],
+    tags: ['test', 'hello'],
+    task: function (cb) {
+      log('Hello');
+      cb();
+    }
+  },
+  {
+    depends: ['hello', 'space'],
+    tags: ['test', 'world'],
+    task: function (cb) {
+      log('world!');
+      cb();
+    }
+  },
+  {
+    depends: ['hello'],
+    tags: ['test', 'space'],
+    task: function (cb) {
+      log('_');
+      cb();
+    }
+  },
+  {
+    depends: ['hello', 'space', 'world'],
+    tags: [],
+    task: function (cb) {
+      log('Hello world!');
+      cb();
+    }
+  },
+  {
+    name: 'hello tags',
+    depends: ['test'],
+    tags: [],
+    task: function (cb) {
+      log('Hello tags!');
+      cb();
+    }
+  }
 ];
 
 Yantr(tasks).run(function () {
+  log(arguments);
   log('Done!');
 });
