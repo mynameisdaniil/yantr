9b6962bbb36735fd1274437dc49ca591741483b5
Daniil Sobol <sobol.daniil@gmail.com>
2014-07-05 01:53:13 +0400

    refactoring

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var Yaff  = require('yaff');
 var maybe = require('maybe2');
-var log   = console.log;
 
 var Yantr = module.exports = function Yantr(tasks) {
   if (!(this instanceof Yantr))
@@ -20,25 +19,24 @@ var Yantr = module.exports = function Yantr(tasks) {
 Yantr.prototype.run = function (cb) {
   var self = this;
   Yaff(this.tasks).parEach(function (task) {
-    self.__taskExecutor(task, this);
+    __taskExecutor(task, self.index, this);
   }).finally(cb);
 };
 
-Yantr.prototype.__taskExecutor = function (task, cb) {
-  var self = this;
-  if (task.exec)
+var __isDone = function (tag, index, cb) {
+  if (index[tag].every(function (task) {
+    return task.done;
+  }))
     return cb();
-  task.exec = true;
-  Yaff(task.depends || []).parEach(function (dependency) {
-    self.__execByTag(dependency, this);
+  setImmediate(function () {
+    __isDone(tag, index, cb);
+  });
+};
+
+var __taskExecutor = function (task, index, cb) {
+  Yaff(task.depends || []).parEach(function (tag) {
+    __isDone(tag, index, this);
   }).seq(function () {
     task.payload(this);
-  }).finally(cb);
-};
-
-Yantr.prototype.__execByTag = function (tag, cb) {
-  var self = this;
-  Yaff(this.index[tag] || []).parEach(function (task) {
-    self.__taskExecutor(task, this);
-  }).finally(cb);
+  }).dummy(task.done = true).finally(cb);
 };
